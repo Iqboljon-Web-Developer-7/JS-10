604a8aa58b75740de81738e6d21e1aa18b7029ae
Iqboljon <iqboljon@Iqboljons-MacBook-Pro.local>
2024-07-23 21:34:18 +0500

    h

--- product/app.js
+++ product/app.js
@@ -17,13 +17,7 @@ function createItem(data) {
         <p>${data.description}</p>
         <img src="${data.images[0]}" width="200" alt="">
 
-        <div className="imgs">
-            ${data.images.forEach((item, idx) => {
-              let img = document.createElement("img");
-              img.src = item;
-              return img;
-            })}
-        </div>
+        
 
         <button>Buy now</button>
     `;
